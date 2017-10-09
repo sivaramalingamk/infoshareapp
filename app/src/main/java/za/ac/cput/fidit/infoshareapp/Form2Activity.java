202989cea27621613b731c6fce8b3658af43f276
ADMIN <glodi.atoba@gmail.com>
2017-10-10 01:01:55 +0200

    Java code for form 2 activity. if radiobutton click it brings to password activity

--- app/src/main/java/za/ac/cput/fidit/infoshareapp/Form2Activity.java
+++ app/src/main/java/za/ac/cput/fidit/infoshareapp/Form2Activity.java
@@ -1,6 +1,8 @@
 package za.ac.cput.fidit.infoshareapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +12,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class Form2Activity extends AppCompatActivity {
-
+    RadioGroup rgroup ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,27 @@ public class Form2Activity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                rgroup = (RadioGroup) findViewById(R.id.radioGroup);
+                rgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                        RadioButton rb = (RadioButton) rgroup.findViewById(i);
+
+                        if (rb.isChecked()){
+                            Form2Activity.this.startActivity(new Intent(Form2Activity.this, PasswordActivity.class));
+
+
+                        }
+
+                    }
+                });
+
 
 
             }
         });
+
+
     }
 
 
